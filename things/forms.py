c9d363172b8bc58bc32bd2dd5ddaaca5a0c6c167
Romina <k22034001@kcl.ac.uk>
2023-10-15 16:36:36 +0100

    fix minor things

--- things/forms.py
+++ things/forms.py
@@ -4,9 +4,10 @@ from .models import Thing
 
 
 class ThingForm(forms.ModelForm):
-    description = forms.Textarea()
-    quantity = forms.NumberInput()
-
     class Meta:
         model = Thing
         fields = ['name', 'description', 'quantity']
+        widgets = {
+            'description': forms.Textarea(),
+        }
+    quantity = forms.NumberInput()
