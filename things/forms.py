97f212823a1e0b61073271a0652548bbb1531021
Romina <k22034001@kcl.ac.uk>
2023-10-15 16:05:33 +0100

    Add fields conditions

--- things/forms.py
+++ things/forms.py
@@ -3,10 +3,9 @@ from django import forms
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 class ThingForm(forms.Form):
-    name=forms.CharField(max_length=35, unique=True, blank=False)
-    description=forms.Textarea(max_length=120, blank=True, unique=False)
+    name=forms.CharField(max_length=35)
+    description=forms.Textarea(max_length=120)
     quantity = forms.IntegerField(
-        unique=False,
         validators=[
             MinValueValidator(0),
             MaxValueValidator(50)
