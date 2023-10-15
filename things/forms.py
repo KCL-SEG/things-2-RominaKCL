5584df340011a2d3a09ec066f440a4e5a8230a9e
Romina <k22034001@kcl.ac.uk>
2023-10-15 16:04:03 +0100

    Add fields conditions

--- things/forms.py
+++ things/forms.py
@@ -1,9 +1,14 @@
 """Forms of the project."""
 from django import forms
-from django.core.validators import MinLengthValidator
-from django.core.validators import MaxLengthValidator
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class ThingForm(forms.Form):
-    name=forms.CharField(max_length=35)
-    description=forms.Textarea()
-    quantity=forms.NumberInput(min_value=0, max_value=50)
+    name=forms.CharField(max_length=35, unique=True, blank=False)
+    description=forms.Textarea(max_length=120, blank=True, unique=False)
+    quantity = forms.IntegerField(
+        unique=False,
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(50)
+        ]
+    )
